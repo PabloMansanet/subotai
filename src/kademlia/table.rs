70f40b04d8265bd24979dd22e6bc4b7c534f5a34
PabloMansanet <pablomansanet@eprosima.com>
2016-06-14 15:37:23 +0200

    table lookup

--- src/kademlia/table.rs
+++ src/kademlia/table.rs
@@ -2,11 +2,17 @@ use hash::KEY_SIZE;
 use hash::Hash160;
 use std::net;
 use std::collections::VecDeque;
+use std::mem;
 
-const BUCKET_DEPTH : usize = 20;
+const K : usize = 20;
+const BUCKET_DEPTH : usize = K;
 
 /// Kademlia routing table, with 160 buckets of `BUCKET_DEPTH` (k) node
 /// identifiers each.
+///
+/// The structure employs least-recently seen eviction. Conflicts generated
+/// by evicting a node by inserting a newer one remain tracked, so they can
+/// can be resolved later.
 pub struct RoutingTable {
    parent_key : Hash160,
    buckets    : Vec<Bucket>,
@@ -21,6 +27,7 @@ struct Bucket {
 
 /// Represents a conflict derived from attempting to insert a node in a full
 /// bucket. 
+#[derive(Debug,Clone)]
 struct EvictionConflict {
    evicted  : NodeInfo,
    inserted : NodeInfo
@@ -28,9 +35,15 @@ struct EvictionConflict {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct NodeInfo {
-   key  : Hash160,
-   ip   : net::IpAddr,
-   port : u16,
+   pub key  : Hash160,
+   pub ip   : net::IpAddr,
+   pub port : u16,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum LookupResult {
+   Found(NodeInfo), 
+   ClosestNodes(Vec<NodeInfo>)
 }
 
 impl RoutingTable {
@@ -41,13 +54,15 @@ impl RoutingTable {
       }
 
       RoutingTable { 
-         parent_key : parent_key.clone(),
+         parent_key : parent_key,
          buckets : buckets,
          conflicts  : Vec::new() 
       }
    }
 
    /// Inserts a node in the routing table. Employs least-recently-seen eviction
+   /// by kicking out the oldest node in case the bucket is full, and registering
+   /// an eviction conflict that can be revised later.
    pub fn insert_node(&mut self, info : NodeInfo) {
       if let Some(index) = self.bucket_for_node(&info.key) {
          let bucket = &mut self.buckets[index];
@@ -59,8 +74,14 @@ impl RoutingTable {
             self.conflicts.push(conflict);
          }
 
-            bucket.entries.push_back(info);
+         bucket.entries.push_back(info);
       }
+   }
+
+   /// Performs a node lookup on the routing table. The lookup result may
+   /// contain the specific node, or a list of up to the K closest nodes;
+   pub fn lookup(&self, key : &Hash160) -> LookupResult {
+      unimplemented!();
    }
 
    /// Returns a table entry for the specific node with a given hash.
@@ -79,6 +100,13 @@ impl RoutingTable {
        Hash160::xor_distance(&self.parent_key, key).height()
    }
 
+   fn revert_conflict(&mut self, conflict : EvictionConflict) {
+      if let Some(index) = self.bucket_for_node(&conflict.inserted.key) {
+         let bucket  = &mut self.buckets[index];
+         let evictor = &mut bucket.entries.iter_mut().find(|ref info| conflict.inserted.key == info.key).unwrap();
+         mem::replace::<NodeInfo>(evictor, conflict.evicted);
+      }
+   }
 }
 
 impl Bucket {
@@ -117,14 +145,8 @@ mod tests {
        assert_eq!(table.specific_node(&node_info.key), Some(node_info));
     }
 
-    #[test]
-    fn inserting_in_a_full_bucket_causes_eviction_conflict() {
-       let mut parent_key = Hash160::blank();
-       parent_key.raw[1] = 1; // This will guarantee all nodes will fall on the same bucket.
+    fn fill_with_low_keys(table : &mut RoutingTable) {
        let any_ip = net::IpAddr::from_str("0.0.0.0").unwrap();
-
-       let mut table = RoutingTable::new(parent_key);
-
        for i in 0..super::BUCKET_DEPTH {
           let mut key = Hash160::blank();
           key.raw[0] = i as u8;
@@ -135,9 +157,20 @@ mod tests {
           };
           table.insert_node(info);
        }
+    }
+
+    #[test]
+    fn inserting_in_a_full_bucket_causes_eviction_conflict() {
+       let mut parent_key = Hash160::blank();
+       parent_key.raw[1] = 1; // This will guarantee all nodes will fall on the same bucket.
+       let any_ip = net::IpAddr::from_str("0.0.0.0").unwrap();
+
+       let mut table = RoutingTable::new(parent_key);
+
+       fill_with_low_keys(&mut table);
        assert!(table.conflicts.is_empty());
 
-       // When we add another node to the same bucket, we cause a conflict
+       // When we add another node to the same bucket, we cause a conflict.
        let mut key = Hash160::blank();
        key.raw[0] = 0xFF;
        let info = NodeInfo { 
@@ -153,5 +186,48 @@ mod tests {
        assert_eq!(conflict.evicted.key, Hash160::blank());
        // We inserted the newest node, but it remains referenced in the conflict.
        assert!(table.specific_node(&conflict.inserted.key).is_some());
+    }
+
+    #[test]
+    fn reverting_an_eviction_conflict_reinserts_the_evicted_node_in_place_of_evictor() {
+       let mut parent_key = Hash160::blank();
+       parent_key.raw[1] = 1; // This will guarantee all nodes will fall on the same bucket.
+       let any_ip = net::IpAddr::from_str("0.0.0.0").unwrap();
+
+       let mut table = RoutingTable::new(parent_key);
+
+       fill_with_low_keys(&mut table);
+       assert!(table.conflicts.is_empty());
+
+       // When we add another node to the same bucket, we cause a conflict.
+       let mut key = Hash160::blank();
+       key.raw[0] = 0xFF;
+       let info = NodeInfo { 
+          key  : key,
+          ip   : any_ip.clone(),
+          port : 0
+       };
+       table.insert_node(info);
+       assert_eq!(table.conflicts.len(), 1);
+       let conflict = table.conflicts.pop().unwrap();
+
+       table.revert_conflict(conflict.clone());
+       // The evictor has been removed.
+       assert!(table.specific_node(&conflict.inserted.key).is_none());
+       // And the evicted has been reinserted.
+       assert!(table.specific_node(&conflict.evicted.key).is_some());
+    }
+
+    #[test]
+    fn lookup_for_a_stored_node() { 
+       let mut table = RoutingTable::new(Hash160::random());
+       let node = NodeInfo {
+          key  : Hash160::random(),
+          ip   : net::IpAddr::from_str("0.0.0.0").unwrap(),
+          port : 0,
+       };
+       table.insert_node(node.clone());
+
+       assert_eq!(table.lookup(&node.key), LookupResult::Found(node));
     }
 }
