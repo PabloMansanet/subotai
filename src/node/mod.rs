50437275b1df93b5963110a1a093ce724f9233b5
Cu3rv0 <pablomansanet@gmail.com>
2016-06-21 22:25:51 +0200

    Ping api

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -4,7 +4,8 @@ use rpc;
 use hash::Hash;
 use bincode::serde;
 use std::{net, io, thread, time};
-use std::sync::{ Weak, Arc};
+use std::sync;
+use std::sync::{Weak, Arc};
 
 pub const SOCKET_BUFFER_SIZE_BYTES : usize = 65536;
 pub const SOCKET_TIMEOUT_S         : u64   = 5;
@@ -22,9 +23,10 @@ struct Resources {
    table    : routing::Table,
    outbound : net::UdpSocket,
    inbound  : net::UdpSocket,
-   state    : State,
+   state    : sync::Mutex<State>,
 }
 
+#[derive(Eq, PartialEq)]
 enum State {
    Alive,
    Error,
@@ -40,7 +42,7 @@ impl Node {
          table      : routing::Table::new(id),
          inbound    : try!(net::UdpSocket::bind(("0.0.0.0", inbound_port))),
          outbound   : try!(net::UdpSocket::bind(("0.0.0.0", outbound_port))),
-         state      : State::Alive,
+         state      : sync::Mutex::new(State::Alive),
       });
 
       try!(resources.inbound.set_read_timeout(Some(time::Duration::new(SOCKET_TIMEOUT_S,0))));
@@ -51,27 +53,24 @@ impl Node {
       Ok( Node{ resources: resources } )
    }
 
-   /// Sends an RPC to a destination node. If the node address is known, the RPC will
-   /// be sent directly. Otherwise, a node lookup will be performed first.
-   pub fn send_rpc(&self, rpc: &rpc::Rpc, destination: &routing::NodeInfo)-> io::Result<()> {
-      if let Some(address) = destination.address {
-         let payload = rpc.serialize();
-         try!(self.resources.outbound.send_to(&payload, address));
-      }
-      else {  
-         // Perform lookup.
-         unimplemented!();  
-      }
-      Ok(())
+   pub fn ping(&self, destination: routing::NodeInfo) {
+      let resources = self.resources.clone();
+      thread::spawn(move || {
+         if let Some(address) = destination.address {
+            let ping = rpc::Rpc::ping(resources.id.clone(), resources.inbound.local_addr().unwrap().port());
+            let payload = ping.serialize();
+            resources.outbound.send_to(&payload, address);
+         }
+      });
    }
 
-   /// Receives and processes data as long as the table is alive. Will gracefully exit, at most,
-   /// `SOCKET_TIMEOUT_S` seconds after the table is dropped.
+
+   /// Receives and processes data as long as the table is alive.
    fn reception_loop(weak: Weak<Resources>) {
       let mut buffer = [0u8; SOCKET_BUFFER_SIZE_BYTES];
 
       while let Some(strong) = weak.upgrade() {
-         if let State::ShuttingDown = strong.state {
+         if let State::ShuttingDown = *strong.state.lock().unwrap() {
             break;
          }
 
@@ -79,6 +78,12 @@ impl Node {
             strong.process_incoming_rpc(&buffer, source);
          }
       }
+   }
+}
+
+impl Drop for Node {
+   fn drop(&mut self) {
+      *self.resources.state.lock().unwrap() = State::ShuttingDown;
    }
 }
 
@@ -129,13 +134,12 @@ mod tests {
          node_id : beta.resources.id.clone(),
          address : Some(address_beta),
       };
-      let ping = rpc::Rpc::ping(alpha.resources.id.clone(), 50000);
 
       // Before sending the ping, beta does not know about alpha.
       assert!(beta.resources.table.specific_node(&alpha.resources.id).is_none());
 
       // Alpha pings beta.
-      alpha.send_rpc(&ping, &info_beta);
+      alpha.ping(info_beta);
 
       // Eventually, beta knows of alpha.
       let mut found = beta.resources.table.specific_node(&alpha.resources.id);
