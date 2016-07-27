6a12d74a8918dccbbf62b00bb97316c018d09ea7
PabloMansanet <pablomansanet@gmail.com>
2016-07-27 16:59:26 +0200

    Docs

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -13,45 +13,54 @@ pub struct Rpc {
 }
 
 impl Rpc {
+   /// Constructs a ping RPC. Pings simply carry information about the
+   /// sender, and expect a response indicating that the receiving node
+   /// is alive.
    pub fn ping(sender_id: Hash, reply_port: u16) -> Rpc {
       Rpc { kind: Kind::Ping, sender_id: sender_id, reply_port: reply_port }
    }
 
+   /// Constructs a ping response. 
    pub fn ping_response(sender_id: Hash, reply_port: u16) -> Rpc {
       Rpc { kind: Kind::PingResponse, sender_id: sender_id, reply_port: reply_port }
    }
 
-   /// Asks for a the results of a table node lookup.
+   /// Constructs an RPC asking for a the results of a table node lookup.
    pub fn find_node(sender_id: Hash, reply_port: u16, id_to_find: Hash, nodes_wanted: usize) -> Rpc {
       let payload = Arc::new(FindNodePayload { id_to_find: id_to_find, nodes_wanted: nodes_wanted });
       Rpc { kind: Kind::FindNode(payload), sender_id: sender_id, reply_port: reply_port }
    }
 
-   /// Encapsulates the response to a previously requested find_node RPC.
+   /// Constructs an RPC with the response to a find_node RPC.
    pub fn find_node_response(sender_id: Hash, reply_port: u16, id_to_find: Hash, result: routing::LookupResult) -> Rpc {
       let payload = Arc::new(FindNodeResponsePayload { id_to_find: id_to_find, result: result} );
       Rpc { kind: Kind::FindNodeResponse(payload), sender_id: sender_id, reply_port: reply_port }
    }
 
+   /// Constructs a bootstrap RPC. It asks the receiving node to provide a list of
+   /// nodes close to the sender, to facilitate joining the network.
    pub fn bootstrap(sender_id: Hash, reply_port: u16) -> Rpc {
       Rpc { kind: Kind::Bootstrap, sender_id: sender_id, reply_port: reply_port }
    }
 
+   /// Constructs the response to a bootstrap RPC.
    pub fn bootstrap_response(sender_id: Hash, reply_port: u16, nodes: Vec<routing::NodeInfo>) -> Rpc {
       let payload = Arc::new(BootstrapResponsePayload { nodes: nodes } );
       Rpc { kind: Kind::BootstrapResponse(payload), sender_id: sender_id, reply_port: reply_port }
    }
 
+   /// Serializes an RPC to be send over TCP. 
    pub fn serialize(&self) -> Vec<u8> {
        serde::serialize(&self, bincode::SizeLimit::Bounded(node::SOCKET_BUFFER_SIZE_BYTES as u64)).unwrap()
    }
 
+   /// Deserializes into an RPC structure.
    pub fn deserialize(serialized: &[u8]) -> serde::DeserializeResult<Rpc> {
        serde::deserialize(serialized)
    }
 
    /// Reports whether the RPC is a FindNodeResponse looking
-   /// for a particular node
+   /// for a particular node.
    pub fn is_finding_node(&self, id: &Hash) -> bool {
       match self.kind {
          Kind::FindNodeResponse( ref payload ) => &payload.id_to_find == id,
@@ -59,7 +68,7 @@ impl Rpc {
       }
    }
    /// Reports whether the RPC is a FindNodeResponse that found
-   /// a particular node
+   /// a particular node.
    pub fn found_node(&self, id: &Hash) -> bool {
       if let Kind::FindNodeResponse(ref payload) = self.kind {
          match payload.result {
